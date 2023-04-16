dd7669fbe3aef9c55ad21e475f07b88f6aaee9fe
ashutuptiwari <106370097+ashutuptiwari@users.noreply.github.com>
2023-04-16 19:25:00 +0530

    Update trial1.py

--- trial1.py
+++ trial1.py
@@ -16,25 +16,25 @@ def generate_frames():
         if not ret:
             break
 
-        # Analyze emotion using DeepFace
+       
         try:
-            # Convert frame to RGB format expected by DeepFace
+           
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            # Analyze emotions in the frame
+            
             result = DeepFace.analyze(rgb_frame, actions=['emotion'],enforce_detection=False)
 
-            # Get the dominant emotion from the results
+            
             
             dominant_emotion = result[0]['dominant_emotion']
 
         except Exception as e:
             dominant_emotion = e
 
-        # Render frame with dominant emotion as text overlay
+        
         cv2.putText(frame, dominant_emotion, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-        # Encode frame as JPEG imag
+        
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
